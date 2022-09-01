4fb0fe3708dff4e125a2c78f2827706dc75dacdb
ANDRESSA BEATRIZ FIDELIS LANDI COELHO <22282076@CFP127ALUNO.LOCAL>
2022-09-01 11:19:00 -0300

    primeira tentativa

---
+++ src/senai/jandira/br/codPrincipal.java
@@ -0,0 +1,55 @@
+package senai.jandira.br;
+
+import java.util.Scanner;
+
+public class codPrincipal {
+
+	public static void main(String[] args) {
+		
+		//leitura
+		Scanner leitor = new Scanner(System.in);
+		
+		int multiplicando ;
+		
+		int minimoMultiplicador;
+		
+		int maximoMultiplicador;
+		
+		int resultado;
+		
+		System.out.println("multiplicando?");
+		multiplicando=leitor.nextInt();
+				
+		System.out.println(" minimo multiplicador");
+		minimoMultiplicador=leitor.nextInt();
+		
+		System.out.println("maximo multiplicador");
+		maximoMultiplicador = leitor.nextInt();
+		
+		//equação
+		
+	   
+	   
+	   while ( minimoMultiplicador <= maximoMultiplicador) {
+		   
+		   resultado = multiplicando * minimoMultiplicador;
+		   
+		   System.out.println(resultado);
+		   
+		   minimoMultiplicador++;
+		
+		
+	   }
+	   
+		
+		
+         
+         
+		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+		
+		
+		
+		
+	}
+
+}
